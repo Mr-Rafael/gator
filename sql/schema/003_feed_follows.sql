13845a30131277fede18b1b9d76c6ac380d4b816
Mr-Rafael <rafael.mazariegosr@gmail.com>
2025-10-14 22:03:04 -0600

    added feed follows table migration

---
+++ sql/schema/003_feed_follows.sql
@@ -0,0 +1,14 @@
+-- +goose Up
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE feed_follows (
+    id uuid PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    user_id uuid NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    feed_id uuid NOT NULL REFERENCES feeds(id) ON DELETE CASCADE,
+    UNIQUE (user_id, feed_id)
+);
+
+-- +goose Down
+DROP TABLE feed_follows;
